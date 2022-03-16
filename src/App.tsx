1397b62135e0f753052e13ee2efa271e89532097
GuHyeon <54255662+guhyeon-kwon@users.noreply.github.com>
2022-03-16 17:00:35 +0900

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import Header from "./Components/Header";
 import Home from "./Routes/Home";
 import Search from "./Routes/Search";
 import Tv from "./Routes/Tv";
+
 function App() {
     return (
         <Router>
-            <Header />
+            <Header/>
             <Switch>
                 <Route path="/tv">
-                    <Tv />
+                    <Tv/>
                 </Route>
                 <Route path="/search">
-                    <Search />
+                    <Search/>
                 </Route>
-                <Route path="/">
-                    <Home />
+                <Route path={["/", "/movies/:movieId"]}>
+                    <Home/>
                 </Route>
             </Switch>
         </Router>
     );
 }
+
 export default App;

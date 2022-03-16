6ec8f5371b2ce19987c170e9ea43a854f7b38607
GuHyeon <54255662+guhyeon-kwon@users.noreply.github.com>
2022-03-17 00:45:30 +0900

    commit

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -87,11 +87,35 @@ const BigMovie = styled(motion.div)`
   position: absolute;
   width: 40vw;
   height: 80vh;
-  background-color: red;
   left: 0;
   right: 0;
   margin: 0 auto;
+  border-radius: 15px;
+  overflow: hidden;
+  background-color: ${(props) => props.theme.black.lighter};
 `
+
+const BigCover = styled.div`
+  width: 100%;
+  background-size: cover;
+  background-position: center center;
+  height: 400px;
+`;
+
+const BigTitle = styled.h3`
+  color: ${(props) => props.theme.white.lighter};
+  padding: 20px;
+  font-size: 46px;
+  position: relative;
+  top: -80px;
+`;
+
+const BigOverview = styled.p`
+  padding: 20px;
+  position: relative;
+  top: -80px;
+  color: ${(props) => props.theme.white.lighter};
+`;
 
 const rowVariants = {
     hidden: {
@@ -151,6 +175,9 @@ function Home() {
         history.push(`/movies/${movieId}`);
     }
     const onOverlayClick = () => history.push("/")
+    const clickedMovie =
+        bigMovieMatch?.params.movieId &&
+        data?.results.find((movie) => movie.id === +bigMovieMatch.params.movieId);
     return (
         <Wrapper>
             {isLoading ? (
@@ -201,7 +228,20 @@ function Home() {
                             <>
                                 <Overlay onClick={onOverlayClick} exit={{opacity: 0}} animate={{opacity: 1}} />
                                 <BigMovie style={{top: scrollY.get() + 100}} layoutId={bigMovieMatch.params.movieId}>
-                                    Hello
+                                    {clickedMovie && (
+                                        <>
+                                            <BigCover
+                                                style={{
+                                                    backgroundImage: `linear-gradient(to top, black, transparent), url(${makeImagePath(
+                                                        clickedMovie.backdrop_path,
+                                                        "w500"
+                                                    )})`,
+                                                }}
+                                            />
+                                            <BigTitle>{clickedMovie.title}</BigTitle>
+                                            <BigOverview>{clickedMovie.overview}</BigOverview>
+                                        </>
+                                    )}
                                 </BigMovie>
                             </>
                         ) : null}
